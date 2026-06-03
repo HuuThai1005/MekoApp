ee110e05f0f531cf67d3ed5103afaf11873aab5a
HuuThai <thainh.ce190375@gmail.com>
2026-06-03 13:52:47 +0700

    Initial commit

---
+++ src/main/java/Meko/MekoApp/MekoAppApplication.java
@@ -0,0 +1,13 @@
+package Meko.MekoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MekoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MekoAppApplication.class, args);
+	}
+
+}
